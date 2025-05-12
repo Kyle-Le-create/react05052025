486add59616da68b5882021f58399e411bada557
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2025-05-12 15:27:13 -0500

    Update on homework

---
+++ learning/week1/day5JSBasic2/functions.js
@@ -0,0 +1,25 @@
+export function sum(a = 0, b = 0) {
+  // write a function that takes two numbers as arguments and returns their sum
+  // argument default values are 0
+  // if wrong data type is passed, throw an error
+  // Define your function here
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Wrong input type");
+  } else {
+    return a + b;
+  }
+  // return () => {};
+}
+
+export function sumOfAll(...args) {
+  // write a function that takes any number of arguments and returns their sum
+  // if wrong data type is passed, throw an error
+  // Define your function here
+  return args.reduce((total, num) => {
+    if (typeof num !== "number") {
+      throw new Error("Wrong input type");
+    }
+    return total + num;
+  }, 0);
+  // return () => {};
+}
