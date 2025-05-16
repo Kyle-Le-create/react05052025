11ca3f0c2da7a6ce3956f6241c6f5835d0314f0d
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2025-05-16 11:22:18 -0500

    Update on my project

---
+++ learning/week2/day7advancedfunction/arrayprototype.js
@@ -0,0 +1,31 @@
+// Custom map
+Array.prototype.myMap = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
+
+// Custom filter
+Array.prototype.myFilter = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+// Custom reduce
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : this[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+
+  return accumulator;
+};
